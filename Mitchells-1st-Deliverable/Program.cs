530bf9b4da5f80bf37bee137387147f64b6ebd51
Mitchell Owen <mitchy7632@gmail.com>
2022-01-14 22:11:21 -0700

    Finished Project

--- Mitchells-1st-Deliverable/Program.cs
+++ Mitchells-1st-Deliverable/Program.cs
@@ -6,7 +6,33 @@ namespace Mitchells_1st_Deliverable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while(true)
+            {
+                Console.WriteLine("How many PB & J Sandwiches do you need?");
+                double sandwiches = Double.Parse(Console.ReadLine());
+                Console.WriteLine($"You want {sandwiches} sandwiches");
+                double numOfBread = (sandwiches * 2) / 28;
+                double loaves = Math.Ceiling(numOfBread);
+
+                double numOfPB = (sandwiches * 2) / 32;
+                double pb = Math.Ceiling(numOfPB);
+
+                double numOfJelly = (sandwiches * 4) / 48;
+                double jelly = Math.Ceiling(numOfJelly);
+
+                Console.WriteLine($"You'll need {loaves} loaves");
+                Console.WriteLine($"You'll need {pb} peanut butter jars");
+                Console.WriteLine($"You'll need {jelly} jars of jelly");
+
+                Console.WriteLine("Would you like to make more sandwiches");
+                string answer = Console.ReadLine();
+                if (!(answer == "yes" || answer == "y"))
+                {
+                    Console.WriteLine("Enjoy your sandwiches!");
+                    break;
+                }
+            }
+
         }
     }
 }
